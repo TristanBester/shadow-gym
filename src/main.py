a2047e324328a6a3462868f0f5f398ed469e0fd3
TristanBester <tristanbester@gmail.com>
2025-11-03 17:00:12 +0200

    feat: added basic control

--- src/main.py
+++ src/main.py
@@ -2,8 +2,33 @@ import time
 
 import hydra
 import mujoco
+import numpy as np
 from gymnasium.envs.mujoco.mujoco_rendering import MujocoRenderer
 from omegaconf import DictConfig
+
+
+def sample_action():
+    action = np.random.uniform(-1, 1, size=(20,))
+    return np.ones_like(action)
+
+
+def set_action(action, model, data):
+    clipped_action = np.clip(action, -1, 1)
+
+    # Get the control ranges for each actuator in the model
+    control_ranges = model.actuator_ctrlrange
+
+    # Compute the half the range of actuation for each actuator
+    half_actuation_ranges = (control_ranges[:, 1] - control_ranges[:, 0]) / 2.0
+
+    # Compute the center of the actuation for each actuator
+    actuation_centers = (control_ranges[:, 1] + control_ranges[:, 0]) / 2.0
+
+    # Compute the control signal associated with the provided action
+    control = actuation_centers + clipped_action * half_actuation_ranges
+
+    data.ctrl[:] = control
+    return data
 
 
 @hydra.main(
@@ -18,6 +43,9 @@ def main(config: DictConfig):
     renderer = MujocoRenderer(model=model, data=data, default_cam_config=config.camera)
 
     while True:
+        action = sample_action()
+        data = set_action(action, model, data)
+
         mujoco.mj_step(model, data)
         renderer.render("human")
         time.sleep(0.01)
